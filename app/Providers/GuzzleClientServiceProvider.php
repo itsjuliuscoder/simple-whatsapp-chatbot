721c387e31fc3f225a46295be18c8edfd2c46481
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2020-08-22 07:37:26 +0100

    First commit for my simple bots app

---
+++ app/Providers/GuzzleClientServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class GuzzleClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
